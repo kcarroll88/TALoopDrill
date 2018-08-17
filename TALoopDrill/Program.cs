dccad1db4de0dc4f60e877f2a1afa5bdd9ffcced
Keenan Carroll <keenan.carroll.va@gmail.com>
2018-08-17 17:59:28 -0400

    Add project files.

---
+++ TALoopDrill/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TALoopDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
